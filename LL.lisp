f6e7a1338d58b4f1adb92c9a1415874eb2e9968c
Brooks Rady <b.j.rady@gmail.com>
2014-10-17 12:39:21 -0600

    DNA and Gene parser functions added

--- LL.lisp
+++ LL.lisp
@@ -29,5 +29,19 @@
   (push (make-instance 'std-cell :POS POS :ATP ATP :NA NA :AA AA 
                                 :FA FA :G G :DNA DNA) *cells*))
 
-(defmacro fetch-value (accessor lst index)
-  `(,accessor (nth ,index (reverse ,lst))))
+(defmacro fetch-value (accessor index)
+  `(,accessor (nth ,index (reverse *cells*))))
+
+(defun parse-gene (gene)
+  (let ((gene-val 0))
+    (dotimes (i (length gene))
+      (setf gene-val (+ (* (expt 2 i) (nth i (reverse gene))) gene-val))) gene-val))
+      
+(defun parse-DNA (DNA-seq)
+  (let ((DNA-val nil))
+    (dotimes (i (length DNA-seq))
+      (push (parse-gene (nth i DNA-seq)) DNA-val)) (reverse DNA-val)))
+
+;;(defun dump-ci () Work in progress, dump all cell info in a formatted form.
+;;  (dotimes (i (length *cells*))
+;;  (format t "~%Cell: ~a~%Position: ~a~%Nucleic Acids:"))
