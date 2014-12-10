0dc07d642a5da436220652362b16aa41e8767b39
Brooks Rady <b.j.rady@gmail.com>
2014-12-09 22:25:16 -0700

    Inital commit

---
+++ GeneR.lisp
@@ -0,0 +1,18 @@
+(defun G0 (celli var)
+  (let ((Gval (parse-gene (nth 0 (fetch-value :DNA celli *cells*))))
+        (Chlorop nil))
+    (cond ((btwn Gval 0 9)
+           (setf Chlorop 1))
+          ((btwn Gval 10 19)
+           (setf Chlorop 2))
+          ((btwn Gval 20 29)
+           (setf Chlorop 3))
+          (t 
+           (setf Chlorop 1))) ;;This is the "default value". AKA if the gene is not expressed.
+    (cond ((equal var 'chlorop)
+           Chlorop))))
+
+(defun GeneR (celli var)
+  (cond ((equal var 'chlorop)
+    (let ))
+  ))
